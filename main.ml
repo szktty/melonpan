d4c62eed347a5c13642828a88ef60bcbfe05f7a3
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-08-22 03:24:11 +0900

    impl

--- main.ml
+++ main.ml
@@ -1,2 +1,41 @@
+module Process = struct
+
+  type ('a, 'b) t = {
+    mutable pid : int;
+    name : string option;
+    target : ('a -> unit) option;
+    args : 'a option;
+    kwargs : (string * 'b) list;
+    daemon : bool;
+    mutable is_alive : bool;
+  }
+
+  let create ?name ?target ?args ?(kwargs=[]) ?(daemon=false) () =
+    { pid = 0; name; target; args; kwargs; daemon; is_alive = false }
+
+  let run p =
+    p.pid <- Unix.fork ();
+    begin match (p.target, p.args) with
+      | Some f, Some args -> f args
+      | _ -> ()
+    end;
+    ()
+
+  let start p =
+    run p
+
+  let join p =
+    ()
+
+end
+
 let () =
+  Printf.printf "current pid = %d\n" (Unix.getpid ());
+  let f name =
+    Printf.printf "current pid = %d\n" (Unix.getpid ());
+    Printf.printf "hello %s\n" name
+  in
+  let p = Process.create ~target:f ~args:"bob" () in
+  Process.run p;
+  Process.join p;
   ()
