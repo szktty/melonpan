1d33f41be699d5a4749537215e0a9f23bbfeabdc
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2017-08-24 11:23:53 +0900

    impl

--- option.ml
+++ option.ml
@@ -1,3 +1,20 @@
+let is_none = function
+  | None -> true
+  | Some _ -> false
+
+let is_some opt =
+  not @@ is_none opt
+
 let value_exn = function
   | None -> failwith "none"
   | Some value -> value
+
+let value opt ~default =
+  match opt with
+  | None -> default
+  | Some value -> value
+
+let value_map opt ~default ~f =
+  match opt with
+  | None -> default
+  | Some value -> f value
